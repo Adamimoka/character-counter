8a1b7654a890a1c2f243337b8a238f7cca8f5d78
Adamimoka <adamimoka@gmail.com>
2024-12-16 18:51:40 -0500

    added tooltips and more display info

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ function countLetters(text, countMode) {
     // Mode 1: Counts if letter appears at all (in "that": t=1)
 
     let letterCount = {};
+    let letterFraction = {};
+
     if (countMode == 0) {
         for (const word of text) {
             for (const letter of word) {
@@ -13,7 +15,11 @@ function countLetters(text, countMode) {
                     letterCount[letter] = 1;
                 }
             }
-        }      
+        }
+        const totalLetters = Object.values(letterCount).reduce((a, b) => a + b, 0);
+        for (const letter in letterCount) {
+            letterFraction[letter] = letterCount[letter] / totalLetters;
+        }
     } else {
         const lettersToCheck = new Set(text.join(""));
         for (const word of text) {
@@ -23,20 +29,29 @@ function countLetters(text, countMode) {
                         letterCount[letter] += 1;
                     } else {
                         letterCount[letter] = 1;
-                    }                
+                    }
                 }
             }
         }
+        const totalWords = text.length;
+        for (const letter in letterCount) {
+            letterFraction[letter] = letterCount[letter] / totalWords;
+        }
     }
-    return letterCount;
+    return [letterCount, letterFraction];
 }
 
-function displayLetterCount(letterCountResults, tableID) {
+function displayLetterCount(letterResults, tableID) {
     // letterCountResults: a dictionary with letters as keys and counts as values
     // tableID: the ID of the table to display the results
-    
-    letterCountResults = Object.fromEntries( // Sort the dictionary by value
+    let letterCountResults = letterResults[0];
+    let letterFractionResults = letterResults[1];
+
+    letterCountResults = Object.fromEntries( // Sort the dictionaries by value
         Object.entries(letterCountResults).sort(([,a],[,b]) => b-a).reverse()
+    );
+    letterFractionResults = Object.fromEntries(
+        Object.entries(letterFractionResults).sort(([,a],[,b]) => b-a).reverse()
     );
 
     const table = document.getElementById(tableID);
@@ -45,15 +60,30 @@ function displayLetterCount(letterCountResults, tableID) {
         const row = table.insertRow(0);
         const cell1 = row.insertCell(0);
         const cell2 = row.insertCell(1);
+        const cell3 = row.insertCell(2);
         cell1.innerHTML = letter;
         cell2.innerHTML = letterCountResults[letter];
+        cell3.innerHTML = (letterFractionResults[letter]*100).toFixed(2) + "%";
     }
-    table.insertRow(0).innerHTML = "<th>Letter</th><th>Count</th>";
+
+    let countExplanation = "";
+    let percentExplanation = "";
     if (tableID === "table0") {
-        table.insertRow(0).innerHTML = "<th colspan='2'>Number of appearances (in \"that\": t=2)</th>";
+        countExplanation = "Number of times the letter appears in the text"
+        percentExplanation = "Percent of all all letters in the text that are this letter"
     }
     else if (tableID === "table1") {
-        table.insertRow(0).innerHTML = "<th colspan='2'>Letter appears in word (in \"that\": t=1)</th>";
+        countExplanation = "Number of words that contain the letter"
+        percentExplanation = "Percent of all words that contain the letter"
+    }
+    const countText = 'Count <div class="tooltip">(??)<span class="tooltiptext">' + countExplanation + '</span></div>'
+    const percentText = 'Percentage <div class="tooltip">(??)<span class="tooltiptext">' + percentExplanation + '</span></div>'
+    table.insertRow(0).innerHTML = "<th>Letter</th><th>" + countText + "</th><th>" + percentText + "</th>";
+    if (tableID === "table0") {
+        table.insertRow(0).innerHTML = "<th colspan='3'>Number of appearances</th>";
+    }
+    else if (tableID === "table1") {
+        table.insertRow(0).innerHTML = "<th colspan='3'>Appearance per word</th>";
     }
 }
 
@@ -86,9 +116,9 @@ function processTextInput(textInput) {
     }
     textInput = textInput.split(/\s+/);
 
-    const letterCountResultsType0 = countLetters(textInput , 0);
-    const letterCountResultsType1 = countLetters(textInput , 1);
+    const letterCountResults0 = countLetters(textInput , 0);
+    const letterCountResults1 = countLetters(textInput , 1);
 
-    displayLetterCount(letterCountResultsType0, "table0");
-    displayLetterCount(letterCountResultsType1, "table1");
+    displayLetterCount(letterCountResults0, "table0");
+    displayLetterCount(letterCountResults1, "table1");
 }
